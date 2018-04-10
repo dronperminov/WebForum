e423af3126116487cf08de37f76a23e202ab229a
dronperminov <dronperminov@gmail.com>
2018-04-10 11:21:33 +0300

    Fix sql creating script

--- src/sql_scripts/create.sql
+++ src/sql_scripts/create.sql
@@ -1,3 +1,10 @@
+use webforum;
+
+drop table messages;
+drop table topic;
+drop table user;
+drop table sections;
+
 CREATE TABLE IF NOT EXISTS `webforum`.`user` (
   `userId` INT(11) NOT NULL,
   `surname` VARCHAR(45) NULL DEFAULT NULL,
@@ -12,8 +19,29 @@ CREATE TABLE IF NOT EXISTS `webforum`.`user` (
   PRIMARY KEY (`userId`),
   UNIQUE INDEX `login_UNIQUE` (`login` ASC))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
+DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE IF NOT EXISTS `webforum`.`sections` (
+  `sectionId` INT(11) NOT NULL,
+  `sectionName` VARCHAR(300) NOT NULL,
+  PRIMARY KEY (`sectionId`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `webforum`.`topic` (
+  `topicId` INT(11) NOT NULL,
+  `sectionId` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(500) NULL DEFAULT NULL,
+  `description` VARCHAR(3000) NULL DEFAULT NULL,
+  PRIMARY KEY (`topicId`),
+  INDEX `sectionId_idx` (`sectionId` ASC),
+  CONSTRAINT `sectionIdForeignKey`
+    FOREIGN KEY (`sectionId`)
+    REFERENCES `webforum`.`sections` (`sectionId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `webforum`.`messages` (
   `messageId` INT(11) NOT NULL,
@@ -35,29 +63,5 @@ CREATE TABLE IF NOT EXISTS `webforum`.`messages` (
     REFERENCES `webforum`.`user` (`userId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-
-
-CREATE TABLE IF NOT EXISTS `webforum`.`topic` (
-  `topicId` INT(11) NOT NULL,
-  `sectionId` INT(11) NULL DEFAULT NULL,
-  `name` VARCHAR(500) NULL DEFAULT NULL,
-  `description` VARCHAR(3000) NULL DEFAULT NULL,
-  PRIMARY KEY (`topicId`),
-  INDEX `sectionId_idx` (`sectionId` ASC),
-  CONSTRAINT `sectionIdForeignKey`
-    FOREIGN KEY (`sectionId`)
-    REFERENCES `webforum`.`sections` (`sectionId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-
-
-CREATE TABLE IF NOT EXISTS `webforum`.`sections` (
-  `sectionId` INT(11) NOT NULL,
-  `sectionName` VARCHAR(300) NOT NULL,
-  PRIMARY KEY (`sectionId`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
